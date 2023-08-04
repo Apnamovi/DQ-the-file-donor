1563a20a4fbd38900aa3fda78e2c6792adb766cf
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-08-04 19:38:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Apnamovi/DQ-the-file-donor.git /DQ-the-file-donor
+  git clone https://github.com/Apnamovi/DQ-the-file-donor.git /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-the-file-donor
+  git clone $UPSTREAM_REPO /DQTheFileDonorBot
 fi
 cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
